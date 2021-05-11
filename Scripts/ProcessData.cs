c9e36d3b5956e51c847660f196368ca2158d68fa
woowon123 <wlsdndnjs12@naver.com>
2021-05-11 15:30:56 +0900

    Add Temp Class file
    Add Temp Class file

---
+++ Scripts/ProcessData.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProcessData
+{
+    //Process
+    /********************************************************************/
+    public class DCST
+    {
+        [SerializeField] public string What;
+        [SerializeField] public string How;
+        [SerializeField] public string Time;
+        [SerializeField] public string Status;
+    }
+    /********************************************************************/
+
+
+    //Tools
+    /********************************************************************/
+    public class Tools
+    {
+        [SerializeField] public List<cTool> Whats;
+    }
+
+    public class cTool
+    {
+        [SerializeField] public string Category;
+        [SerializeField] public string Color;
+        [SerializeField] public string Brand;
+    }
+    /********************************************************************/
+
+
+    //obj
+    /********************************************************************/
+    public class MyBusket
+    {
+        [SerializeField] public List<mObject> Whats;
+    }
+
+    public class mObject
+    {
+        [SerializeField] public int Count;
+        [SerializeField] public int Amount;
+        [SerializeField] public string Status;
+        [SerializeField] public string BuyingDate;
+        [SerializeField] public string ExpireDate;
+    }
+    /********************************************************************/
+}
---
+++ Scripts/ProcessMng.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//by jin 2021.05.11
+//Purpose : Manged by Queue & Stack logic
+public class ProcessMng : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
