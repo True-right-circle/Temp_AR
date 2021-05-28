6c5f4bc60a8c482c815edb0d2bd251d86583a7f2
woowon123 <wlsdndnjs12@naver.com>
2021-05-28 18:51:58 +0900

    Add folder
    Add folder

--- Assets/Scripts/Inputsysmng.cs
+++ Assets/Scripts/Inputsysmng.cs
@@ -3,11 +3,65 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
+using System.Globalization;
+
+//올림 : Math.ceiling()
+//내림 : Math.Truncate
+//반올림 : Math.Round(값,자릿수)
 public class Inputsysmng : MonoBehaviour
 {
+    //M 단위
+    private float pivotSize = 3.305785f;
+    private float width = 0f;
+    private float height = 0f;
+    private float totalSize = 0.0f;
+    private string pnum = "";
+
+    public Canvas PlaneCanvas;
+    public GameObject Cude_Panel;
+    public GameObject Default_Cude; 
+    public List<GameObject> Cubes;
 
     public void OnTextEnd(TextMeshProUGUI num)
     {
-        UnityEngine.Debug.Log("num == "+ num.text);
+        Cubes = new List<GameObject>();
+
+        //num뒤에 공백이 온다?
+        //https://forum.unity.com/threads/textmesh-pro-ugui-hidden-characters.505493/
+        pnum = num.text.Trim((char)8203);
+        totalSize = int.Parse(pnum) * pivotSize;
     }
+    public void OnTextEndWidth(TextMeshProUGUI num)
+    {
+        string swidth = num.text.Trim((char)8203);
+        UnityEngine.Debug.Log(swidth);
+        width = float.Parse(swidth);
+        height = totalSize / width;
+        double dheight = Math.Round(totalSize / width,2);
+        height = (float)dheight;
+        UnityEngine.Debug.Log("Total = " + totalSize);
+        UnityEngine.Debug.Log("width = " + width);
+        UnityEngine.Debug.Log("height = " + height);
+        UnityEngine.Debug.Log("height = " + dheight);
+        BuildPlane();
+    }
+
+    private void BuildPlane()
+    {
+        if (!PlaneCanvas.gameObject.activeSelf)
+            PlaneCanvas.gameObject.SetActive(true);
+
+        PlaneCanvas.GetComponent<RectTransform>().sizeDelta = new Vector2(width, height);
+        TurnOnCude();
+    }
+
+    public void TurnOnCude()
+    {
+        for(int i=0; i<(int)totalSize; ++i)
+        {
+            Cude_Panel.transform.GetChild(i).gameObject.SetActive(true);
+        }
+    }
+
 }
--- Assets/Scripts/Roundary.cs
+++ Assets/Scripts/Roundary.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Roundary : MonoBehaviour
 {
+    private float pivotSize = 3.305785f;
+
     // Start is called before the first frame update
     void Start()
     {
