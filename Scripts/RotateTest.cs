b77f4e39317b93e56595663dc67957beecf35cca
woowon123 <wlsdndnjs12@naver.com>
2021-06-01 14:48:24 +0900

    Edit Rotate with Camera.main position
    1. Camera Lerp position
    2. GameObject Rotate

--- Scripts/RotateTest.cs
+++ Scripts/RotateTest.cs
@@ -4,21 +4,35 @@ using UnityEngine;
 
 public class RotateTest : MonoBehaviour
 {
+    public GameObject mCam;
     public GameObject mPlane;
     private bool isTop = false;
+    private bool isCamTop = false;
+
     Quaternion Top = new Quaternion();
     Quaternion Front = new Quaternion();
+    Vector3 CamFront = new Vector3();
+    Vector3 CamTop = new Vector3();
+
+    Coroutine CamRotateCor = null;
     Coroutine RotateCor = null;
-    float spinSpeed = 250f;
+    float spinSpeed = 600f;
     private void Awake()
     {
         Top = Quaternion.Euler(-90,0,0);
         Front = Quaternion.Euler(0,45,0);
+
+        CamFront = new Vector3(0, 3.5f, -10);
+        CamTop = new Vector3(0, 0, -10);
+
+        float angle = Quaternion.Angle(mPlane.transform.localRotation, mCam.transform.localRotation);
+        UnityEngine.Debug.Log("cam r " + angle);
     }
 
     public void OnClickRotatePlane()
     {
-       startRotateCor();
+        startRotateCor();
+        //StartCamRotateCor();
     }
 
     private void startRotateCor()
@@ -39,7 +53,11 @@ public class RotateTest : MonoBehaviour
                 angle = Quaternion.Angle(mPlane.transform.localRotation, Front);
                 yield return null;
             }
-
+            while (!(mCam.transform.localPosition == CamFront))
+            {
+                mCam.transform.localPosition = Vector3.Lerp(mCam.transform.localPosition, CamFront, Time.deltaTime * 10);
+                yield return null;
+            }
             isTop = false;
         }
         else
@@ -51,7 +69,52 @@ public class RotateTest : MonoBehaviour
                 angle = Quaternion.Angle(mPlane.transform.localRotation, Top);
                 yield return null;
             }
+            while(!(mCam.transform.localPosition== CamTop))
+            {
+                mCam.transform.localPosition = Vector3.Lerp(mCam.transform.localPosition, CamTop, Time.deltaTime * 10);
+                yield return null;
+            }
             isTop = true;
         }
     }
+
+
+    private void StartCamRotateCor()
+    {
+        if (CamRotateCor != null)
+            CamRotateCor = null;
+
+        CamRotateCor = StartCoroutine(RotateCamCorutine());
+
+    }
+
+    IEnumerator RotateCamCorutine()
+    {
+        if(isCamTop)
+        {
+            float angle = Quaternion.Angle(mPlane.transform.localRotation, mCam.transform.localRotation);
+            while (angle<95.0f && angle>45.0f)
+            {
+                mCam.transform.RotateAround(mPlane.transform.position, Vector3.left, Time.deltaTime * 300);
+                angle = Quaternion.Angle(mPlane.transform.localRotation, mCam.transform.localRotation);
+                UnityEngine.Debug.Log("cam r " + angle);
+                yield return null;
+            }
+            isCamTop = false;
+        }
+        else
+        {
+            float angle = Quaternion.Angle(mPlane.transform.localRotation, mCam.transform.localRotation);
+            while (angle<90.0f)
+            {
+                mCam.transform.RotateAround(mPlane.transform.position, Vector3.right, Time.deltaTime * 600);
+                angle = Quaternion.Angle(mPlane.transform.localRotation, mCam.transform.localRotation);
+                yield return null;
+            }
+           // UnityEngine.Debug.Log("cam r " + angle);
+            isCamTop = true;
+        }
+    }
+
+
 }
