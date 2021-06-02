c734f83de92fd83009663d2ed2652eb672998f14
woowon123 <wlsdndnjs12@naver.com>
2021-06-02 17:18:07 +0900

    Add  ScreenPointToLocalPointInRectangle 주석
    1. ScreenPointToLocalPointInRectangle 설명 주석

--- Scripts/MousePositionDebug.cs
+++ Scripts/MousePositionDebug.cs
@@ -18,8 +18,12 @@ public class MousePositionDebug : MonoBehaviour, IPointerClickHandler
 
     //https://answers.unity.com/questions/1169455/convert-inputmouseposition-to-recttransform-pivot.html
     //Input -> Mouse Position
+    //ScreenPointToLocalPointInRectangle
+    //param : 점이 위치할 사각형, 마우스(터치포인터) 위치, 카메라 : 메인 카메라, out -> 결과물 좌표
+    //화면 공간 포인트를 사각형 평면에있는 RectTransform의 로컬 공간 위치로 변환합니다.
     public Vector2 ScreenToRectPos(Vector2 screen_pos)
     {
+        //Canvas Mode => World
         if (canvas.renderMode != RenderMode.ScreenSpaceOverlay && canvas.worldCamera != null)
         {
             //Canvas is in Camera mode
