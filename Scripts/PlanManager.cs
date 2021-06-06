b7cab6acb337675d9a410b1d24f9a47de550e855
woowon123 <wlsdndnjs12@naver.com>
2021-06-06 22:09:10 +0900

    1. Add To do Func
    1. Add To do Func

--- Scripts/PlanManager.cs
+++ Scripts/PlanManager.cs
@@ -2,6 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Mode
+{
+    ROtate,
+    Block,
+    Shit
+}
+//Plane Edit Mode
 public class PlanManager : MonoBehaviour
 {
     public static PlanManager Instance;
@@ -10,5 +17,28 @@ public class PlanManager : MonoBehaviour
     {
         if (Instance == null)
             Instance = this;
+    }
+
+    //back
+    public void OnClickUndo()
+    {
+
+    }
+
+    //Delete
+    public void Alldelete()
+    {
+
+    }
+
+    //Save
+    public void Save()
+    {
+
+    }
+
+    public void ModeChange(Mode m)
+    {
+
     }
 }
